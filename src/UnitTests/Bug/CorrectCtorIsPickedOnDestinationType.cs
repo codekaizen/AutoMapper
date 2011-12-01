27e877124f237236a22388f4312efa4376f1bd69
codekaizen <codekaizen@gmail.com>
2011-12-01 12:30:19 -0800

    Adding Kelly's unit test which demonstrates the ctor picking issue

---
+++ src/UnitTests/Bug/CorrectCtorIsPickedOnDestinationType.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    using System;
+
+    [TestFixture]
+    public class CorrectCtorIsPickedOnDestinationType : AutoMapperSpecBase
+    {
+        public enum SomeEnum
+        {
+            First,
+            Second,
+            Third
+        }
+
+        public class SourceClass { }
+
+        public class DestinationClass
+        {
+            public DestinationClass()
+            {
+            }
+
+            public DestinationClass(Int32 type)
+            {
+                TestType = type;
+            }
+
+            public Int32 TestType { get; private set; }
+        }
+
+        [Test]
+        public void Should_pick_a_ctor_which_best_matches()
+        {
+            Mapper.CreateMap<SourceClass, DestinationClass>();
+
+            var source = new SourceClass();
+
+            object dest = Mapper.Map<DestinationClass>(source);
+        }
+    }
+}
